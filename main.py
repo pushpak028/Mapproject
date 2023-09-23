54f9c3fd9002e34ef38180dd87d5b7582e9c6b41
Pushpak Raj <67819822+pushpak028@users.noreply.github.com>
2023-09-23 11:28:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,86 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy_garden.mapview import MapView,MapSource,MapMarker,MapMarkerPopup
-import nearby_station
-from kivy.uix.label import Label
-from kivy.uix.button import Button
+import discord
+from discord.ext import commands 
+from discord import Intents , client
+import loady
+import distancefinder
+import findloc
+import re
+import waythrough
 
-class PoliceApp(App):
-    def build(self):
-        box_layout = BoxLayout()
-        map_view = MapView(lat=13.1345613974711, lon=80.24637305490332,zoom = 15)
-        map_view.map_source = "osm"
-        lat1,lon1,phone = self.police_nearby(13.1345613974711,80.24637305490332)
-        map_marker = MapMarkerPopup(lat = lat1 , lon = lon1 , source = "images/marker1.png")
-        self.value_label = Label(text=f"phone:{phone}",size=(200,100) )
-        btn = Button(size =( 0.2,0.05),pos =(map_marker.pos[0],map_marker.pos[1]))
-        map_marker.add_widget(btn)
-        map_view.add_widget(map_marker)
-        box_layout.add_widget(map_view)
-        box_layout.add_widget(self.value_label)
-        return box_layout
-    
+def app_run(token):
+    intents = Intents.default()
+    intents.message_content = True
+    bot = commands.Bot(command_prefix='!',intents=intents)
+    data:dict = loady.load1("data/intents.json")
+    coordinate_pattern = r'(-?\d+\.\d+),\s*(-?\d+\.\d+)'
 
+    @bot.event
+    async def on_ready():
+        print(f"{bot.user} is now running")
 
     
-    def police_nearby(self,lat,lon):
-        list1 = nearby_station.get_nearby_police_station(lat,lon)
-        latitude = list1[0][0]
-        longitude = list1[0][1]
-        phone = list1[0][2]
-        return latitude,longitude,phone
+    @bot.command()
+    async def findPolice(ctx,lat,long):
+        lat = float(lat)
+        long = float(long)
+        result = distancefinder.nearbystation(lat,long)
+        latitude = result[2]
+        longitude = result[3]
+        name = result[1]
+        image = findloc.findy(latitude,longitude,name,lat,long)
 
 
+        with open("images/output.png","rb") as im1:
+            file1 = discord.File(im1 ,"output.png" )
+        
+        channel_id = 1153583047380303912
+        channel = bot.get_channel(channel_id)
+
+        await channel.send(result)
+        await channel.send(file=file1)
+    
+    @bot.command()
+    async def wayfinder(ctx,lat,lon):
+        lat1 = float(lat)
+        lon1 = float(lon)
+        result = distancefinder.nearbystation(lat1,lon1)
+        lat2 = result[2]
+        lon2 = result[3]
+        image1 = waythrough.wayy(lat1,lon1,lat2,lon2)
+
+        with open("images/outputd.png","rb") as im2:
+            file1 = discord.File(im2,"waytogo.png")
+        
+        channel_id = 1154632947375276083 
+        channel = bot.get_channel(channel_id)
+
+        
+        await channel.send(result[0])
+        await channel.send(file=file1)
+
+    
+
+    @bot.event
+    async def on_message(message):
+        if bot.user == message.author:
+            return
+        
+        
+        channel_id = 1147062396926562336
+        channel = bot.get_channel(channel_id)
+        
+        match = re.search(coordinate_pattern , message.content)
+        if match:
+            latitude = float(match.group(1))
+            longitude = float(match.group(2))
+            await channel.send(latitude,longitude)
+        
+        print(f"({bot.user} {message.author}):{message.content}")
+        
+        await bot.process_commands(message)
+
+    bot.run(token)
 
 if __name__ == "__main__":
-    PoliceApp().run()
+    token = "your-token"
+    app_run(token)
